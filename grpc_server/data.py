84f1052210e8eaa905f486a07d120e3c67a793cb
walter lacoste <wlacoste@outlook.com>
2022-09-14 17:00:56 -0300

    consumer python

--- grpc_server/data.py
+++ grpc_server/data.py
@@ -1,13 +1,13 @@
 from faker import Faker
 from kafka import KafkaProducer
 import json
-
+import time
 
 def json_serializer(data):
-    return json.dumps(data).encode
+    return json.dumps(data).encode("utf-8")
 
 
-producer = KafkaProducer(boostrap_servers=['127.0.0.1:9092'],
+producer = KafkaProducer(bootstrap_servers=['127.0.0.1:9092'],
                         value_serializer=json_serializer)
 
 
@@ -25,7 +25,10 @@ def get_user():
 
 if __name__ == "__main__":
     print(get_user())
-    for i in range(10):
+    while 1 ==1 :
         user = get_user()
+        print(user)
+        producer.send("TestTopic", user)
+        time.sleep(4)
 
 
