8689b6f47cef83fee5415b360b2efe4d30a37bb8
magostino <martinagostino75@yahoo.com.ar>
2022-09-23 21:40:36 -0300

    Update database.sql

--- database.sql
+++ database.sql
@@ -10,7 +10,7 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,N
 -- -----------------------------------------------------
 -- Schema retroshop
 -- -----------------------------------------------------
-/*DROP SCHEMA IF EXISTS `retroshop` ;*/
+DROP SCHEMA IF EXISTS `retroshop` ;
 
 -- -----------------------------------------------------
 -- Schema retroshop
@@ -29,13 +29,12 @@ CREATE TABLE IF NOT EXISTS `retroshop`.`usuario` (
   `apellido` VARCHAR(45) NOT NULL,
   `dni` BIGINT(8) UNSIGNED ZEROFILL NOT NULL,
   `email` VARCHAR(45) NOT NULL,
-  `esmonitor` tinyint DEFAULT 0 NOT NULL,
+  `esmonitor` TINYINT NOT NULL DEFAULT '0',
   `usuario` VARCHAR(45) NOT NULL,
   `contraseña` VARCHAR(45) NOT NULL,
   `saldo` FLOAT UNSIGNED NOT NULL DEFAULT '0',
   PRIMARY KEY (`idusuario`))
 ENGINE = InnoDB
-AUTO_INCREMENT = 16
 DEFAULT CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_0900_ai_ci;
 
@@ -55,7 +54,31 @@ CREATE TABLE IF NOT EXISTS `retroshop`.`carrito` (
     FOREIGN KEY (`cliente_idusuario`)
     REFERENCES `retroshop`.`usuario` (`idusuario`))
 ENGINE = InnoDB
-AUTO_INCREMENT = 6
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `retroshop`.`factura`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `retroshop`.`factura` ;
+
+CREATE TABLE IF NOT EXISTS `retroshop`.`factura` (
+  `idfactura` INT NOT NULL AUTO_INCREMENT,
+  `fechacompra` DATETIME NULL DEFAULT NULL,
+  `vendedor_idusuario` INT NOT NULL,
+  `idcarrito` INT NOT NULL,
+  `totalfacturado` FLOAT NULL,
+  PRIMARY KEY (`idfactura`),
+  INDEX `fk_vendedor_usuario1_idx` (`vendedor_idusuario` ASC) VISIBLE,
+  INDEX `fk_factura_producto_carrito_idx` (`idcarrito` ASC) VISIBLE,
+  CONSTRAINT `fk_factura_carrito`
+    FOREIGN KEY (`idcarrito`)
+    REFERENCES `retroshop`.`carrito` (`idcarrito`),
+  CONSTRAINT `fk_factura_usuario1`
+    FOREIGN KEY (`vendedor_idusuario`)
+    REFERENCES `retroshop`.`usuario` (`idusuario`))
+ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_0900_ai_ci;
 
@@ -69,61 +92,6 @@ CREATE TABLE IF NOT EXISTS `retroshop`.`tipo_categoria` (
   `idtipocategoria` INT NOT NULL AUTO_INCREMENT,
   `nombre` VARCHAR(45) NULL DEFAULT NULL,
   PRIMARY KEY (`idtipocategoria`))
-ENGINE = InnoDB
-AUTO_INCREMENT = 6
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
-
--- -----------------------------------------------------
--- Table `retroshop`.`factura`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `retroshop`.`factura` ;
-
-CREATE TABLE IF NOT EXISTS `retroshop`.`factura` (
-  `idfactura` INT NOT NULL AUTO_INCREMENT,
-  `fechacompra` DATETIME NULL DEFAULT NULL,
-  `cantidad` FLOAT NULL DEFAULT NULL,
-  `precio` int NULL DEFAULT NULL,
-  `subtotal` FLOAT NULL DEFAULT NULL,
-  `total` FLOAT NULL DEFAULT NULL,
-  `vendedor_idusuario` INT NOT NULL,
-  `cliente_idusuario` INT NOT NULL,
-  PRIMARY KEY (`idfactura`),
-  INDEX `fk_vendedor_usuario1_idx` (`vendedor_idusuario` ASC) VISIBLE,
-  INDEX `fk_cliente_usuario2_idx` (`cliente_idusuario` ASC) VISIBLE,
-  CONSTRAINT `fk_factura_usuario1`
-    FOREIGN KEY (`vendedor_idusuario`)
-    REFERENCES `retroshop`.`usuario` (`idusuario`),
-  CONSTRAINT `fk_factura_usuario2`
-    FOREIGN KEY (`cliente_idusuario`)
-    REFERENCES `retroshop`.`usuario` (`idusuario`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
-
--- -----------------------------------------------------
--- Table `retroshop`.`subasta`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `retroshop`.`subasta` ;
-
-CREATE TABLE IF NOT EXISTS `retroshop`.`subasta` (
-  `idsubasta` INT NOT NULL AUTO_INCREMENT,
-  `idproducto` INT NULL DEFAULT NULL,
-  `preciofinal` FLOAT NULL DEFAULT NULL,
-  `fechainicio` DATETIME NULL DEFAULT NULL,
-  `fechafin` DATETIME NULL DEFAULT NULL,
-  `ultimapuja` DATETIME NULL DEFAULT NULL,
-  `pujador_idusuario` INT DEFAULT NULL,
-  PRIMARY KEY (`idsubasta`),
-  INDEX `fk_idproducto_idx` (`idproducto` ASC) VISIBLE,
- 
-  CONSTRAINT `fk_pujador_idusuario`
-    FOREIGN KEY (`pujador_idusuario`)
-    REFERENCES `retroshop`.`usuario` (`idusuario`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_0900_ai_ci;
@@ -143,7 +111,7 @@ CREATE TABLE IF NOT EXISTS `retroshop`.`producto` (
   `cantidad_disponible` INT NULL DEFAULT NULL,
   `fecha_publicacion` VARCHAR(45) NULL DEFAULT NULL,
   `publicador_idusuario` INT NULL DEFAULT NULL,
-  `esSubasta` tinyint DEFAULT 0 NOT NULL,
+  `esSubasta` TINYINT NOT NULL DEFAULT '0',
   `url_foto1` TEXT NULL DEFAULT NULL,
   `url_foto2` TEXT NULL DEFAULT NULL,
   `url_foto3` TEXT NULL DEFAULT NULL,
@@ -159,10 +127,8 @@ CREATE TABLE IF NOT EXISTS `retroshop`.`producto` (
     FOREIGN KEY (`publicador_idusuario`)
     REFERENCES `retroshop`.`usuario` (`idusuario`))
 ENGINE = InnoDB
-AUTO_INCREMENT = 21
 DEFAULT CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_0900_ai_ci;
-
 
 
 -- -----------------------------------------------------
@@ -176,15 +142,46 @@ CREATE TABLE IF NOT EXISTS `retroshop`.`producto_carrito` (
   `idcarrito` INT NULL DEFAULT NULL,
   `cantidad` INT NULL DEFAULT NULL,
   `subtotal` FLOAT NULL DEFAULT NULL,
+  `idfactura` INT NOT NULL,
   PRIMARY KEY (`idproducto_carrito`),
   INDEX `fk_idproducto_idx` (`idproducto` ASC) VISIBLE,
   INDEX `fk_idcarrito_idx` (`idcarrito` ASC) VISIBLE,
+  INDEX `fk_producto_carrito_factura1_idx` (`idfactura` ASC) VISIBLE,
   CONSTRAINT `fk_idcarrito`
     FOREIGN KEY (`idcarrito`)
     REFERENCES `retroshop`.`carrito` (`idcarrito`),
   CONSTRAINT `fk_idproducto`
     FOREIGN KEY (`idproducto`)
-    REFERENCES `retroshop`.`producto` (`idproducto`))
+    REFERENCES `retroshop`.`producto` (`idproducto`),
+  CONSTRAINT `fk_producto_carrito_factura1`
+    FOREIGN KEY (`idfactura`)
+    REFERENCES `retroshop`.`factura` (`idfactura`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `retroshop`.`subasta`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `retroshop`.`subasta` ;
+
+CREATE TABLE IF NOT EXISTS `retroshop`.`subasta` (
+  `idsubasta` INT NOT NULL AUTO_INCREMENT,
+  `idproducto` INT NULL DEFAULT NULL,
+  `preciofinal` FLOAT NULL DEFAULT NULL,
+  `fechainicio` DATETIME NULL DEFAULT NULL,
+  `fechafin` DATETIME NULL DEFAULT NULL,
+  `ultimapuja` DATETIME NULL DEFAULT NULL,
+  `pujador_idusuario` INT NULL DEFAULT NULL,
+  PRIMARY KEY (`idsubasta`),
+  INDEX `fk_idproducto_idx` (`idproducto` ASC) VISIBLE,
+  INDEX `fk_pujador_idusuario` (`pujador_idusuario` ASC) VISIBLE,
+  CONSTRAINT `fk_pujador_idusuario`
+    FOREIGN KEY (`pujador_idusuario`)
+    REFERENCES `retroshop`.`usuario` (`idusuario`))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_0900_ai_ci;
@@ -194,4 +191,3 @@ INSERT INTO `tipo_categoria` VALUES (1,'Videojuegos'),(2,'Musica'),(3,'Revistas'
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
-
